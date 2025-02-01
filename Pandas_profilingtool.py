83ab08a0c2f72adfe6a55b140b4d9dd5e68415a8
parthgiramkar <imparthgiramkar@gmail.com>
2025-02-01 22:41:49 +0530

    Update Pandas_profilingtool.py

--- Pandas_profilingtool.py
+++ Pandas_profilingtool.py
@@ -4,6 +4,8 @@ df = pd.read_csv('titanic.csv')
 
 df.head()
 
+"""This installs the ydata_profiling library, which is used for automatic data analysis and 
+generating detailed reports & !is used to run this command in a Jupyter Notebook or similar environments"""
 !pip install ydata_profiling
 
 from ydata_profiling import ProfileReport

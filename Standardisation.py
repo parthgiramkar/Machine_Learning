ac0b80b5049712370720e2667a7d4ae3a8fc4997
parthgiramkar <imparthgiramkar@gmail.com>
2025-02-09 16:23:14 +0530

    Update Standardisation.py

--- Standardisation.py
+++ Standardisation.py
@@ -17,6 +17,6 @@
 
 
 
-Pandas DataFrame stores data in tabular form, while .values extracts only  Pandas DataFrame stores data in tabular form, while .values extracts onlythe raw numerical values as a NumPy array.
+Pandas DataFrame stores data in tabular form,  while .values extracts onlythe raw numerical values as a NumPy array.
 Efficient Computations – NumPy operations are faster and consume less memory compared to Pandas operations. 
 Required by Some ML Libraries – Many machine learning libraries (like Scikit-Learn) expect input in the form of a NumPy array instead of a Pandas DataFrame.

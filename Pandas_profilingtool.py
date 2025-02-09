a2aace794a873a00c00e3a4fa70557c583bc5752
parthgiramkar <imparthgiramkar@gmail.com>
2025-02-09 11:19:53 +0530

    Update Pandas_profilingtool.py

--- Pandas_profilingtool.py
+++ Pandas_profilingtool.py
@@ -8,6 +8,7 @@ df.head()
 generating detailed reports & !is used to run this command in a Jupyter Notebook or similar environments"""
 !pip install ydata_profiling
 
-from ydata_profiling import ProfileReport
+from ydata_profiling import ProfileReport       #ydata_profiling is a Python library used for generating automated reports for Exploratory Data Analysis (EDA). 
 prof = ProfileReport(df)
-prof.to_file(output_file='output.html')
+prof.to_file(output_file='output.html')            # exports the generated report into an HTML file named 'output.html opened into any web browser
+

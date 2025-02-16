1e9076b28a1295453bf5a63bd908b993f8b4721c
parthgiramkar <imparthgiramkar@gmail.com>
2025-02-16 15:42:21 +0530

    Update Standardisation.py

--- Standardisation.py
+++ Standardisation.py
@@ -118,21 +118,6 @@ sns.kdeplot(x_train_scaled['EstimatedSalary'], ax=axes2
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Pandas DataFrame stores data in tabular form,  while .values extracts onlythe raw numerical values as a NumPy array.
-Efficient Computations – NumPy operations are faster and consume less memory compared to Pandas operations. 
-Required by Some ML Libraries – Many machine learning libraries (like Scikit-Learn) expect input in the form of a NumPy array instead of a Pandas DataFrame.
+//Pandas DataFrame stores data in tabular form,  while .values extracts onlythe raw numerical values as a NumPy array.
+//Efficient Computations – NumPy operations are faster and consume less memory compared to Pandas operations. 
+//Required by Some ML Libraries – Many machine learning libraries (like Scikit-Learn) expect input in the form of a NumPy array instead of a Pandas DataFrame.
